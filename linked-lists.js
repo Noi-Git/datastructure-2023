d90e181abde304b49a14686a1abe910ac754a7cc
Noi-Git <noi.patsin@gmail.com>
2023-05-22 17:30:28 -0400

    add new node at the tail

--- linked-lists.js
+++ linked-lists.js
@@ -16,10 +16,18 @@ class LinkedList {
 
   // add Node
   append(val) {
+    //create a new node and asigned it to be a new head -- for empty linked list
     if (this.head === null) {
-      this.head = new Node(val) //create a new node and asigned it to be a new head
+      this.head = new Node(val)
       return
     }
+
+    let curr = this.head
+    while (current.nex !== null) {
+      //keep looping until you hit the tail
+      curr = curr.next //set current to its next -- now we find tail
+    }
+    curr.next = new Node(val) //add new node after find the tail
   }
 }
 
