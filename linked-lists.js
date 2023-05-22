98fdadaed7b7a90ba73a413e87c5d6f63b6f8b6b
Noi-Git <noi.patsin@gmail.com>
2023-05-22 17:13:07 -0400

    initialize

---
+++ linked-lists.js
@@ -0,0 +1,18 @@
+/* Linked List */
+
+// Step 1 create node
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+
+  // add Node -- add new node to the end of the linked list
+}
